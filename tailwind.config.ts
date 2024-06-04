c857bcf9c26a5428ba1df138d7c9798a8d64a76d
류정우 <88191233+jw-r@users.noreply.github.com>
2024-06-04 18:08:59 +0900

    feat: review page UI without analysis (#69)
    * feat: 서버 토큰 만료 시, session 제거
    
    * feat: use-media-query
    
    * feat: my-stars-drawer-dialog
    
    * feat: review UI without analysis
    
    * feat: desktop 디자인 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -285,7 +285,11 @@ const config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
+  plugins: [
+    require('tailwindcss-animate'),
+    require('@tailwindcss/typography'),
+    require('tailwind-scrollbar-hide'),
+  ],
 } satisfies Config
 
 export default config
