7faaeb043664f2a28e3d13a2acb959afd714e846
Jungkyun Woo <73513965+woo-jk@users.noreply.github.com>
2024-07-03 01:43:43 +0900

    feat: 모바일 노트 추가하기 플로팅 버튼 구현 (#171)
    * chore: install switch
    
    * feat: switch 디자인 적용
    
    * fix: src 절대경로로 변경
    
    * feat: 프로필 변경 토스트 ui 구현
    
    * feat: 알림 이메일 변경 토스트 ui 구현
    
    * feat: 노트 등록 토스트 ui 구현
    
    * feat: 북마크 설정 토스트 ui 구현
    
    * fix: 인증코드 ui 수정
    
    * fix: 프로필 페이지 ui 수정
    
    * fix: 프로필 정보 수정 페이지 ui 수정
    
    * fix: 노트 창고 페이지 스크롤 방식 변경
    
    * feat: 노트 추가하기 버튼 구현
    
    ---------
    
    Co-authored-by: 류정우 <evencoding@gmail.com>

---
+++ src/components/add-note-floating-button.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import { Button } from './ui/button'
+
+interface Props {
+  categoryId?: number
+}
+
+export default function AddNoteFloatingButton({ categoryId }: Props) {
+  return (
+    <div className="fixed bottom-[120px] right-[20px] drop-shadow-[0_4px_12px_rgba(0,0,0,0.15)] lg:hidden">
+      <Link href={`/create${categoryId ? `?default=${categoryId}` : ''}`}>
+        <Button className="flex h-[48px] w-[136px] items-center gap-[8px] text-body2-bold text-white ">
+          노트 추가하기 <PlusIcon />
+        </Button>
+      </Link>
+    </div>
+  )
+}
+
+function PlusIcon() {
+  return (
+    <svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M6.49902 0V12" stroke="white" stroke-width="1.54011" />
+      <path d="M12.5 6L0.5 6" stroke="white" stroke-width="1.54011" />
+    </svg>
+  )
+}
