da158dc96cd8a088b17bf066cd95f03470a2e268
류정우 <88191233+jw-r@users.noreply.github.com>
2024-07-04 01:25:57 +0900

    feat: root metadata 구현 (#184)
    * feat: root metadata 구현
    
    * chore: add action secrets

---
+++ src/features/metadata.ts
@@ -0,0 +1,21 @@
+import { Metadata } from 'next'
+
+export const Metadatas = {
+  root(): Metadata {
+    return {
+      metadataBase: new URL(`https://www.picktoss.com`),
+      title: '픽토스 | 나만의 AI 퀴즈',
+      description: 'AI로 생성하는 나만의 퀴즈',
+      icons: {
+        icon: '/images/picktoss-logo.png',
+      },
+      openGraph: {
+        url: 'https://www.picktoss.com',
+        images: ['/images/picktoss-logo.png'],
+        title: '똑소 | AI로 생성하는 나만의 퀴즈',
+        description:
+          '나의 노트로부터 생성되는 다양한 퀴즈와 요점 정리를 통해 효율적으로 메타인지 할 수 있어요!',
+      },
+    }
+  },
+}
