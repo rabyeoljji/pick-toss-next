a4d3546b7ae16c6e1dae07d6362bb5f76c2b6464
류정우 <88191233+jw-r@users.noreply.github.com>
2024-09-26 23:44:51 +0900

    feat: text (#209)
    * feat: text
    
    * feat: ref를 받을 수 있도록 수정
    
    * refactor: typography style 전역으로 분리

--- src/app/v3/(routes)/(tabs)/main/page.tsx
+++ src/app/v3/(routes)/(tabs)/main/page.tsx
@@ -1,31 +1,15 @@
-import { Button } from '@/shared/components/ui/button'
+import Text from '@/shared/components/text'
 
 const Home = () => {
   return (
     <div className="flex flex-col gap-[10px] p-[40px]">
-      {/* default variant largeRound, default color primary */}
-      <Button left={<div>ㅁ</div>}>버튼명</Button>
-      <Button variant="mediumIcon" colors="special">
-        X
-      </Button>
-      <Button variant="mediumRound" colors="outlined" right={<div>ㅁ</div>}>
-        버튼명
-      </Button>
-      <Button variant="mediumSquare" colors="primary-loading" left={<div>ㅁ</div>}>
-        버튼명
-      </Button>
-      <Button variant="smallRound" colors="secondary" right={<div>ㅁ</div>}>
-        버튼명
-      </Button>
-      <Button variant="smallSquare" colors="selected" left={<div>ㅁ</div>}>
-        버튼명
-      </Button>
-      <Button variant="tinySquare" colors="tertiary">
-        버튼명
-      </Button>
-      <Button variant="tinySquare" colors="unselected">
-        버튼명
-      </Button>
+      <Text as="p" typography="subtitle1-bold">
+        example
+      </Text>
+      {/* a 태그가 됨 */}
+      <Text as="a" typography="text1-bold" href="" className="">
+        example
+      </Text>
     </div>
   )
 }
