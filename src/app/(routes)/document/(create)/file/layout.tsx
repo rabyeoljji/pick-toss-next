238ec52ac422de7db20b74f7ca5e46e69f52b7e3
rabyeoljji <nageuna922@gmail.com>
2024-12-20 07:36:05 +0000

    fix: provider 중복 사용

--- src/app/(routes)/document/(create)/file/layout.tsx
+++ src/app/(routes)/document/(create)/file/layout.tsx
@@ -1,6 +1,5 @@
 import { FunctionComponent, PropsWithChildren, Suspense } from 'react'
 import type { Metadata } from 'next'
-import { DirectoryProvider } from '@/features/directory/contexts/directory-context'
 import Loading from '@/shared/components/custom/loading'
 
 export const metadata: Metadata = {}
@@ -8,11 +7,7 @@ export const metadata: Metadata = {}
 interface LayoutProps extends PropsWithChildren {}
 
 const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
-  return (
-    <Suspense fallback={<Loading center />}>
-      <DirectoryProvider>{children}</DirectoryProvider>
-    </Suspense>
-  )
+  return <Suspense fallback={<Loading center />}>{children}</Suspense>
 }
 
 export default Layout
