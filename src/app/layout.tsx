9580757a22dae8a48473233fb9eea15f98e97079
rabyeoljji <nageuna922@gmail.com>
2025-01-16 09:06:41 +0000

    fix: pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="picktoss" />
-        <meta name="theme-color" content="#FB8320" />
+        <meta name="theme-color" content="#ffffff" />
         <meta name="background-color" content="#ffffff" />
         <meta name="orientation" content="portrait" />
       </head>
