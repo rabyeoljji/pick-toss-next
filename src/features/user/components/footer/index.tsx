16579ce916043a4baf95cb4a9703a62a4aa308f2
rabyeoljji <nageuna922@gmail.com>
2025-02-17 05:33:09 +0000

    feat: profile footer

--- src/features/user/components/footer/index.tsx
+++ src/features/user/components/footer/index.tsx
@@ -1,11 +1,17 @@
 import Text from '@/shared/components/ui/text'
-import LogoutDialog from '../logout-dialog'
+// import LogoutDialog from '../logout-dialog'
 
 const Footer = () => {
   return (
-    <div className="flex items-center gap-[32px] text-text2-medium text-text-caption">
-      <Text>현재 버전: 13.3.0</Text>
-      <LogoutDialog trigger={<button>로그아웃</button>} />
+    <div className="w-full bg-background-base-02 px-[16px] pb-[73px] pt-[20px]">
+      <Text typography="caption-medium" color="caption" className="mb-[16px]">
+        사업자등록번호 120-20-02237 | 070-7954-1774 | 대표자명 : 이유민 <br /> 서울특별시 서대문구
+        연희로41가길 5, B1층 D72호(홍은동)
+      </Text>
+
+      <Text typography="caption-medium" color="caption">
+        ⓒ 2024. picktoss all rights reserved
+      </Text>
     </div>
   )
 }
