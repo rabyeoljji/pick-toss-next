372974bf299df565771877dfa1dcb9e4e4ce1ef3
rabyeoljji <nageuna922@gmail.com>
2025-02-19 06:02:03 +0000

    fix: notification list loading

--- src/features/notification/components/notification/index.tsx
+++ src/features/notification/components/notification/index.tsx
@@ -7,17 +7,26 @@ import NotificationItem from '../notification-item'
 import { useQuery } from '@tanstack/react-query'
 import { queries } from '@/shared/lib/tanstack-query/query-keys'
 import { useMemo } from 'react'
+import Loading from '@/shared/components/custom/loading'
 
 const Notification = () => {
   const tab = useSearchParams().get('tab') ?? ''
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const activeTab = ['all', 'quiz', 'general'].includes(tab) ? tab : 'all'
-  const { data } = useQuery(queries.notification.all())
+  const { data, isPending } = useQuery(queries.notification.all())
   const notificationList = useMemo(() => data?.notifications || [], [data])
 
   // tab에 따라 서버에 데이터 요청
   // useQuery(() => {
   // }, [activeTab])
+
+  if (isPending) {
+    return (
+      <main className="flex-center h-[calc(100dvh-54px-48px)] w-full px-[16px]">
+        <Loading />
+      </main>
+    )
+  }
 
   return (
     <main className="flex h-[calc(100dvh-54px-48px)] w-full flex-col gap-[8px] overflow-y-auto px-[16px]">
