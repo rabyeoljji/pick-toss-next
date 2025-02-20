1a3a3cac23480880e0238dc111337abd8f9746f3
rabyeoljji <nageuna922@gmail.com>
2025-02-20 11:25:23 +0000

    fix: invite reward beta

--- src/features/user/header.tsx
+++ src/features/user/header.tsx
@@ -5,6 +5,7 @@ import Text from '@/shared/components/ui/text'
 import usePreviousPath from '@/shared/hooks/use-previous-path'
 import { useUserStore } from '@/store/user'
 import Link from 'next/link'
+import InviteRewardDrawer from '../payment/components/invite-reward-drawer'
 
 const Header = () => {
   usePreviousPath()
@@ -14,12 +15,23 @@ const Header = () => {
     <header className="flex h-[54px] w-full max-w-mobile items-center justify-between bg-background-base-01 px-[18px]">
       <Text typography="title2">{user?.name}님</Text>
       <div className="flex-center gap-[16px]">
-        <Link href={'/payment'}>
+        <InviteRewardDrawer
+          triggerComponent={
+            <Text typography="subtitle2-bold" className="flex-center text-text-secondary">
+              <Icon name="star" className="mr-[4px] size-[20px]" />
+              {user?.star}
+            </Text>
+          }
+        />
+
+        {/* TODO: 결제 기능 구현 후 아래 코드로 변경 */}
+        {/* <Link href={'/payment'}>
           <Text typography="subtitle2-bold" className="flex-center text-text-secondary">
             <Icon name="star" className="mr-[4px] size-[20px]" />
             {user?.star}
           </Text>
-        </Link>
+        </Link> */}
+
         <Link href={'/notification'}>
           <Icon name="notification" className="size-[24px]" />
         </Link>
