578b894046787a104f0573690c1631afe30e82b7
rabyeoljji <nageuna922@gmail.com>
2025-02-22 07:56:54 +0000

    fix: intro animation

--- src/shared/components/custom/spring-up-card-animation.tsx
+++ src/shared/components/custom/spring-up-card-animation.tsx
@@ -26,10 +26,10 @@ const SpringUpCardAnimation = ({
           'flex-center h-[420px] w-[320px] flex-col gap-[58px] rounded-[24px] bg-[var(--color-blue-600)]',
           className
         )}
-        initial={{ y: '500px', scaleX: 0, opacity: 0 }}
+        initial={{ y: '700px', scaleX: 0, opacity: 0 }}
         animate={{ y: '0%', scaleX: 1, opacity: 1 }}
         transition={{
-          duration: 0.7,
+          duration: 0.6,
           ease: [0.25, 0.1, 0.25, 1],
         }}
         onAnimationComplete={onAnimationComplete}
@@ -38,12 +38,12 @@ const SpringUpCardAnimation = ({
       <div className="center flex-center h-[420px] w-[320px] flex-col gap-[58px] px-[25px]">
         <motion.div
           className="flex-center flex-col gap-[10px] text-center"
-          initial={{ y: '300px', opacity: 0 }}
+          initial={{ y: '500px', opacity: 0 }}
           animate={{ y: '0%', opacity: 1 }}
           transition={{
-            delay: 0.2,
-            duration: 0.3,
-            ease: [0.25, 0.1, 0.25, 1],
+            delay: 0.1,
+            duration: 0.5,
+            ease: [0.25, 0.1, 0.3, 1],
           }}
         >
           {cardName}
@@ -51,12 +51,12 @@ const SpringUpCardAnimation = ({
         </motion.div>
 
         <motion.div
-          initial={{ y: '100px', opacity: 0 }}
+          initial={{ y: '500px', opacity: 0 }}
           animate={{ y: '0%', opacity: 1 }}
           transition={{
-            delay: 0.3,
-            duration: 0.4,
-            ease: [0.25, 0.1, 0.25, 1],
+            delay: 0.1,
+            duration: 0.5,
+            ease: [0.25, 0.1, 0.3, 1],
           }}
         >
           {image}
