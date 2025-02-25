1cab4f1625d926f195868fe84ad98f6995ee9ff9
rabyeoljji <nageuna922@gmail.com>
2025-02-25 03:29:12 +0000

    fix: document detail page scroll and drawer issue

--- src/features/quiz/components/new-quiz-drawer.tsx
+++ src/features/quiz/components/new-quiz-drawer.tsx
@@ -13,7 +13,6 @@ import { queries } from '@/shared/lib/tanstack-query/query-keys'
 import { calculateAvailableQuizCount } from '@/features/document/utils'
 import { useUserStore } from '@/store/user'
 import { useDocumentDetailContext } from '@/features/document/contexts/document-detail-context'
-import { createPortal } from 'react-dom'
 
 interface Props {
   triggerComponent: React.ReactNode
@@ -63,99 +62,95 @@ const NewQuizDrawer = ({ triggerComponent, documentId, startAddQuizzes }: Props)
 
   return (
     <>
-      <Drawer open={openDrawer} onOpenChange={setOpenDrawer} modal>
+      <Drawer open={openDrawer} onOpenChange={setOpenDrawer}>
         <DrawerTrigger asChild>{triggerComponent}</DrawerTrigger>
+        <DrawerContent
+          overlayProps={{ className: 'max-w-mobile mx-auto' }}
+          className="mx-auto h-fit max-h-[90dvh] max-w-mobile rounded-t-[20px]"
+        >
+          <div className="my-[24px] flex h-fit flex-col items-center overflow-y-auto overflow-x-hidden px-[16px]">
+            <DrawerTitle className="mb-[38px] w-full font-suit text-title3">
+              원하는 유형과 문제 수를 선택해주세요
+            </DrawerTitle>
 
-        {createPortal(
-          <DrawerContent
-            overlayProps={{ className: 'max-w-mobile mx-auto' }}
-            className="mx-auto h-fit max-h-[90dvh] max-w-mobile rounded-t-[20px]"
-          >
-            <div className="my-[24px] flex h-fit flex-col items-center overflow-y-auto overflow-x-hidden px-[16px]">
-              <DrawerTitle className="mb-[38px] w-full font-suit text-title3">
-                원하는 유형과 문제 수를 선택해주세요
-              </DrawerTitle>
-
-              {/* 문제 유형 선택 */}
-              <div className="mb-[28px] flex gap-[8px]">
-                <button
-                  onClick={() => handleClickQuizType('MULTIPLE_CHOICE')}
-                  className={cn(
-                    'flex h-[136px] w-[168px] flex-col justify-end rounded-[16px] border px-[7px] pb-[15px] pt-[20px] focus:border-border-focused focus:bg-background-container-03 focus-visible:outline-none',
-                    quizType === 'MULTIPLE_CHOICE' &&
-                      'bg-background-container-03 border-border-focused'
-                  )}
-                >
-                  <Icon name="multiple-quiz-icon" className="mb-[7.05px] w-[70px]" />
-                  <Text typography="subtitle2-bold" className="mb-[4px] pl-[9px] text-start">
-                    객관식
-                  </Text>
-                  <Text typography="text2-medium" className="pl-[9px] text-start text-text-sub">
-                    4개 선택지 중 정답 고르기
-                  </Text>
-                </button>
-
-                <button
-                  onClick={() => handleClickQuizType('MIX_UP')}
-                  className={cn(
-                    'flex h-[136px] w-[168px] flex-col justify-end rounded-[16px] border pb-[15px] pt-[18px] focus:border-border-focused focus:bg-background-container-03 focus-visible:outline-none',
-                    quizType === 'MIX_UP' && 'bg-background-container-03 border-border-focused'
-                  )}
-                >
-                  <Icon name="o-x-quiz-icon" className="mb-[10px] w-[81px] pl-[14px]" />
-                  <Text typography="subtitle2-bold" className="mb-[4px] pl-[20px] text-start">
-                    O/X
-                  </Text>
-                  <Text typography="text2-medium" className="pl-[20px] text-start text-text-sub">
-                    참과 거짓 판단하기
-                  </Text>
-                </button>
-              </div>
-
-              <div className="flex-center h-fit w-full flex-col border-t pb-[66px] pt-[26px] text-text-sub">
-                <Text typography="text1-medium">만들 문제 수</Text>
-                <Text typography="title1" className="mb-[28px] mt-[8px] text-text-accent">
-                  {quizCount} 문제
+            {/* 문제 유형 선택 */}
+            <div className="mb-[28px] flex gap-[8px]">
+              <button
+                onClick={() => handleClickQuizType('MULTIPLE_CHOICE')}
+                className={cn(
+                  'flex h-[136px] w-[168px] flex-col justify-end rounded-[16px] border px-[7px] pb-[15px] pt-[20px] focus:border-border-focused focus:bg-background-container-03 focus-visible:outline-none',
+                  quizType === 'MULTIPLE_CHOICE' &&
+                    'bg-background-container-03 border-border-focused'
+                )}
+              >
+                <Icon name="multiple-quiz-icon" className="mb-[7.05px] w-[70px]" />
+                <Text typography="subtitle2-bold" className="mb-[4px] pl-[9px] text-start">
+                  객관식
                 </Text>
-
-                {/* 문제 개수 슬라이더 */}
-                <Slider
-                  min={DOCUMENT_MIN_QUIZ_COUNT}
-                  max={DOCUMENT_MAX_QUIZ_COUNT}
-                  step={1}
-                  defaultValue={[DEFAULT_QUIZ_COUNT]}
-                  onValueChange={(value) => setQuizCount(value[0] || DEFAULT_QUIZ_COUNT)}
-                />
-
-                <div className="mt-[10px] flex w-full items-center justify-between text-text2-medium text-text-sub">
-                  <Text>{DOCUMENT_MIN_QUIZ_COUNT} 문제</Text>
-                  <Text>{DOCUMENT_MAX_QUIZ_COUNT} 문제</Text>
-                </div>
-              </div>
-
-              <div className="flex-center w-full flex-col pb-[40px] pt-[21px]">
-                <Text typography="text2-medium">
-                  <span className="text-text-sub">현재 나의 별: </span>
-                  <span className="text-text-secondary">{user?.star}개</span>
+                <Text typography="text2-medium" className="pl-[9px] text-start text-text-sub">
+                  4개 선택지 중 정답 고르기
                 </Text>
+              </button>
 
-                <Button
-                  variant={'largeRound'}
-                  colors={'special'}
-                  className="mt-[5px] w-[335px] max-w-full text-button1 text-text-primary-inverse"
-                  onClick={() => handleClickStart(quizCount, quizType)} // 임시
-                >
-                  퀴즈 시작하기
-                  <div className="flex-center size-[fit] rounded-full bg-[#D3DCE4]/[0.2] px-[8px]">
-                    <Icon name="star" className="mr-[4px] size-[16px]" />
-                    <Text typography="text1-medium">{quizCount}</Text>
-                  </div>
-                </Button>
+              <button
+                onClick={() => handleClickQuizType('MIX_UP')}
+                className={cn(
+                  'flex h-[136px] w-[168px] flex-col justify-end rounded-[16px] border pb-[15px] pt-[18px] focus:border-border-focused focus:bg-background-container-03 focus-visible:outline-none',
+                  quizType === 'MIX_UP' && 'bg-background-container-03 border-border-focused'
+                )}
+              >
+                <Icon name="o-x-quiz-icon" className="mb-[10px] w-[81px] pl-[14px]" />
+                <Text typography="subtitle2-bold" className="mb-[4px] pl-[20px] text-start">
+                  O/X
+                </Text>
+                <Text typography="text2-medium" className="pl-[20px] text-start text-text-sub">
+                  참과 거짓 판단하기
+                </Text>
+              </button>
+            </div>
+
+            <div className="flex-center h-fit w-full flex-col border-t pb-[66px] pt-[26px] text-text-sub">
+              <Text typography="text1-medium">만들 문제 수</Text>
+              <Text typography="title1" className="mb-[28px] mt-[8px] text-text-accent">
+                {quizCount} 문제
+              </Text>
+
+              {/* 문제 개수 슬라이더 */}
+              <Slider
+                min={DOCUMENT_MIN_QUIZ_COUNT}
+                max={DOCUMENT_MAX_QUIZ_COUNT}
+                step={1}
+                defaultValue={[DEFAULT_QUIZ_COUNT]}
+                onValueChange={(value) => setQuizCount(value[0] || DEFAULT_QUIZ_COUNT)}
+              />
+
+              <div className="mt-[10px] flex w-full items-center justify-between text-text2-medium text-text-sub">
+                <Text>{DOCUMENT_MIN_QUIZ_COUNT} 문제</Text>
+                <Text>{DOCUMENT_MAX_QUIZ_COUNT} 문제</Text>
               </div>
             </div>
-          </DrawerContent>,
-          document.body
-        )}
+
+            <div className="flex-center w-full flex-col pb-[40px] pt-[21px]">
+              <Text typography="text2-medium">
+                <span className="text-text-sub">현재 나의 별: </span>
+                <span className="text-text-secondary">{user?.star}개</span>
+              </Text>
+
+              <Button
+                variant={'largeRound'}
+                colors={'special'}
+                className="mt-[5px] w-[335px] max-w-full text-button1 text-text-primary-inverse"
+                onClick={() => handleClickStart(quizCount, quizType)} // 임시
+              >
+                퀴즈 시작하기
+                <div className="flex-center size-[fit] rounded-full bg-[#D3DCE4]/[0.2] px-[8px]">
+                  <Icon name="star" className="mr-[4px] size-[16px]" />
+                  <Text typography="text1-medium">{quizCount}</Text>
+                </div>
+              </Button>
+            </div>
+          </div>
+        </DrawerContent>
       </Drawer>
 
       {/* 이용자에게 별이 부족할 경우 아래 렌더링 */}
--- src/features/quiz/components/replay-quiz-drawer.tsx
+++ src/features/quiz/components/replay-quiz-drawer.tsx
@@ -10,7 +10,6 @@ import { cn } from '@/shared/lib/utils'
 import { useReplayDocumentQuiz } from '@/requests/quiz/hooks'
 import { useRouter } from 'next/navigation'
 import { useDocumentDetailContext } from '@/features/document/contexts/document-detail-context'
-import { createPortal } from 'react-dom'
 
 interface Props {
   triggerComponent: React.ReactNode
@@ -72,122 +71,117 @@ const ReplayQuizDrawer = ({
   }
 
   return (
-    <Drawer open={openDrawer} onOpenChange={setOpenDrawer} modal>
+    <Drawer open={openDrawer} onOpenChange={setOpenDrawer}>
       <DrawerTrigger asChild>{triggerComponent}</DrawerTrigger>
+      <DrawerContent
+        overlayProps={{ className: 'max-w-mobile mx-auto' }}
+        className="mx-auto h-fit max-h-[90dvh] max-w-mobile rounded-t-[20px]"
+      >
+        <div className="my-[24px] flex h-fit flex-col items-center overflow-y-auto overflow-x-hidden px-[16px]">
+          <DrawerTitle className="mb-[38px] w-full font-suit text-title3">
+            원하는 유형과 문제 수를 선택해주세요
+          </DrawerTitle>
 
-      {createPortal(
-        <DrawerContent
-          overlayProps={{ className: 'max-w-mobile mx-auto' }}
-          className="mx-auto h-fit max-h-[90dvh] max-w-mobile rounded-t-[20px]"
-        >
-          <div className="my-[24px] flex h-fit flex-col items-center overflow-y-auto overflow-x-hidden px-[16px]">
-            <DrawerTitle className="mb-[38px] w-full font-suit text-title3">
-              원하는 유형과 문제 수를 선택해주세요
-            </DrawerTitle>
-
-            {/* 문제 유형 선택 */}
-            <div className="mb-[28px] flex gap-[8px]">
-              <button
-                onClick={() => setQuizType('RANDOM')}
-                className={cn(
-                  'flex h-[150px] w-[110px] flex-col justify-end rounded-[16px] border px-[16px] pb-[15px] pt-[20px] focus:border-border-focused focus:bg-background-container-03 focus-visible:outline-none disabled:pointer-events-none disabled:grayscale disabled:bg-background-disabled disabled:text-text-disabled',
-                  quizType === 'RANDOM' && 'bg-background-container-03 border-border-focused'
-                )}
-              >
-                <Icon name="random-quiz-icon" className="mb-[7.05px] w-[76px]" />
-                <Text typography="subtitle2-bold" className="mb-[4px] text-left">
-                  전체
-                </Text>
-                <Text typography="text2-medium" className="flex text-start text-text-sub">
-                  모든 유형 <br /> 랜덤으로 섞기
-                </Text>
-              </button>
-
-              <button
-                disabled={!isIncludeMultiple}
-                onClick={() => setQuizType('MULTIPLE_CHOICE')}
-                className={cn(
-                  'flex h-[150px] w-[110px] flex-col justify-end rounded-[16px] border px-[7px] pb-[15px] pt-[20px] focus:border-border-focused focus:bg-background-container-03 focus-visible:outline-none disabled:bg-border-divider disabled:text-icon-tertiary disabled:border-none',
-                  quizType === 'MULTIPLE_CHOICE' &&
-                    'bg-background-container-03 border-border-focused'
-                )}
-              >
-                <Icon
-                  name={!isIncludeMultiple ? 'disabled-multiple-quiz-icon' : 'multiple-quiz-icon'}
-                  className="mb-[7.05px] w-[70px]"
-                />
-                <Text typography="subtitle2-bold" className="mb-[4px] pl-[9px] text-left">
-                  객관식
-                </Text>
-                <Text
-                  typography="text2-medium"
-                  color={!isIncludeMultiple ? 'caption' : 'sub'}
-                  className="flex pl-[9px] text-start"
-                >
-                  4개 선택지 중 <br /> 정답 고르기
-                </Text>
-              </button>
-
-              <button
-                disabled={!isIncludeMixUp}
-                onClick={() => setQuizType('MIX_UP')}
-                className={cn(
-                  'flex h-[150px] w-[110px] flex-col justify-end rounded-[16px] border pb-[15px] pt-[18px] focus:border-border-focused focus:bg-background-container-03 focus-visible:outline-none disabled:bg-border-divider disabled:text-icon-tertiary disabled:border-none',
-                  quizType === 'MIX_UP' && 'bg-background-container-03 border-border-focused'
-                )}
-              >
-                <Icon
-                  name={!isIncludeMixUp ? 'disabled-o-x-quiz-icon' : 'o-x-quiz-icon'}
-                  className="mb-[10px] w-[81px] pl-[10px]"
-                />
-                <Text typography="subtitle2-bold" className="mb-[4px] pl-[16px] text-left">
-                  O/X
-                </Text>
-                <Text
-                  typography="text2-medium"
-                  color={!isIncludeMixUp ? 'caption' : 'sub'}
-                  className="flex pl-[16px] text-start"
-                >
-                  참과 거짓 <br /> 판단하기
-                </Text>
-              </button>
-            </div>
-
-            <div className="flex-center h-fit w-full flex-col border-t pb-[66px] pt-[26px] text-text-sub">
-              <Text typography="text1-medium">다시 풀 문제 수</Text>
-              <Text typography="title1" className="mb-[28px] mt-[8px] text-text-accent">
-                {quizCount} 문제
+          {/* 문제 유형 선택 */}
+          <div className="mb-[28px] flex gap-[8px]">
+            <button
+              onClick={() => setQuizType('RANDOM')}
+              className={cn(
+                'flex h-[150px] w-[110px] flex-col justify-end rounded-[16px] border px-[16px] pb-[15px] pt-[20px] focus:border-border-focused focus:bg-background-container-03 focus-visible:outline-none disabled:pointer-events-none disabled:grayscale disabled:bg-background-disabled disabled:text-text-disabled',
+                quizType === 'RANDOM' && 'bg-background-container-03 border-border-focused'
+              )}
+            >
+              <Icon name="random-quiz-icon" className="mb-[7.05px] w-[76px]" />
+              <Text typography="subtitle2-bold" className="mb-[4px] text-left">
+                전체
               </Text>
+              <Text typography="text2-medium" className="flex text-start text-text-sub">
+                모든 유형 <br /> 랜덤으로 섞기
+              </Text>
+            </button>
 
-              {/* 문제 개수 슬라이더 */}
-              <Slider
-                min={minQuizCount}
-                max={savedQuizCount} // 저장된 문제 수
-                step={1}
-                defaultValue={[savedQuizCount]} // 저장된 문제 수
-                onValueChange={(value) => setQuizCount(value[0] || 0)}
+            <button
+              disabled={!isIncludeMultiple}
+              onClick={() => setQuizType('MULTIPLE_CHOICE')}
+              className={cn(
+                'flex h-[150px] w-[110px] flex-col justify-end rounded-[16px] border px-[7px] pb-[15px] pt-[20px] focus:border-border-focused focus:bg-background-container-03 focus-visible:outline-none disabled:bg-border-divider disabled:text-icon-tertiary disabled:border-none',
+                quizType === 'MULTIPLE_CHOICE' && 'bg-background-container-03 border-border-focused'
+              )}
+            >
+              <Icon
+                name={!isIncludeMultiple ? 'disabled-multiple-quiz-icon' : 'multiple-quiz-icon'}
+                className="mb-[7.05px] w-[70px]"
               />
-
-              <div className="mt-[10px] flex w-full items-center justify-between text-text2-medium text-text-sub">
-                <Text>{minQuizCount} 문제</Text>
-                <Text>{savedQuizCount} 문제</Text>
-              </div>
-            </div>
-
-            <div className="flex-center w-full flex-col pb-[40px] pt-[21px]">
-              <Button
-                onClick={() => handleClickStart()}
-                variant={'largeRound'}
-                colors={'primary'}
-                className="mt-[5px] w-[335px] max-w-full text-button1 text-text-primary-inverse"
+              <Text typography="subtitle2-bold" className="mb-[4px] pl-[9px] text-left">
+                객관식
+              </Text>
+              <Text
+                typography="text2-medium"
+                color={!isIncludeMultiple ? 'caption' : 'sub'}
+                className="flex pl-[9px] text-start"
               >
-                퀴즈 시작하기
-              </Button>
+                4개 선택지 중 <br /> 정답 고르기
+              </Text>
+            </button>
+
+            <button
+              disabled={!isIncludeMixUp}
+              onClick={() => setQuizType('MIX_UP')}
+              className={cn(
+                'flex h-[150px] w-[110px] flex-col justify-end rounded-[16px] border pb-[15px] pt-[18px] focus:border-border-focused focus:bg-background-container-03 focus-visible:outline-none disabled:bg-border-divider disabled:text-icon-tertiary disabled:border-none',
+                quizType === 'MIX_UP' && 'bg-background-container-03 border-border-focused'
+              )}
+            >
+              <Icon
+                name={!isIncludeMixUp ? 'disabled-o-x-quiz-icon' : 'o-x-quiz-icon'}
+                className="mb-[10px] w-[81px] pl-[10px]"
+              />
+              <Text typography="subtitle2-bold" className="mb-[4px] pl-[16px] text-left">
+                O/X
+              </Text>
+              <Text
+                typography="text2-medium"
+                color={!isIncludeMixUp ? 'caption' : 'sub'}
+                className="flex pl-[16px] text-start"
+              >
+                참과 거짓 <br /> 판단하기
+              </Text>
+            </button>
+          </div>
+
+          <div className="flex-center h-fit w-full flex-col border-t pb-[66px] pt-[26px] text-text-sub">
+            <Text typography="text1-medium">다시 풀 문제 수</Text>
+            <Text typography="title1" className="mb-[28px] mt-[8px] text-text-accent">
+              {quizCount} 문제
+            </Text>
+
+            {/* 문제 개수 슬라이더 */}
+            <Slider
+              min={minQuizCount}
+              max={savedQuizCount} // 저장된 문제 수
+              step={1}
+              defaultValue={[savedQuizCount]} // 저장된 문제 수
+              onValueChange={(value) => setQuizCount(value[0] || 0)}
+            />
+
+            <div className="mt-[10px] flex w-full items-center justify-between text-text2-medium text-text-sub">
+              <Text>{minQuizCount} 문제</Text>
+              <Text>{savedQuizCount} 문제</Text>
             </div>
           </div>
-        </DrawerContent>,
-        document.body
-      )}
+
+          <div className="flex-center w-full flex-col pb-[40px] pt-[21px]">
+            <Button
+              onClick={() => handleClickStart()}
+              variant={'largeRound'}
+              colors={'primary'}
+              className="mt-[5px] w-[335px] max-w-full text-button1 text-text-primary-inverse"
+            >
+              퀴즈 시작하기
+            </Button>
+          </div>
+        </div>
+      </DrawerContent>
     </Drawer>
   )
 }
