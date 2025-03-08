aaae2ff5a42d7dbfa403af1163572728aed86b94
rabyeoljji <nageuna922@gmail.com>
2025-03-08 06:32:29 +0000

    fix: move document drawer

--- src/features/document/components/move-document-drawer/index.tsx
+++ src/features/document/components/move-document-drawer/index.tsx
@@ -16,6 +16,8 @@ import { useState } from 'react'
 import { useDirectoryContext } from '@/features/directory/contexts/directory-context'
 import { useDocumentContext } from '../../contexts/document-context'
 import { useMoveDocument } from '@/requests/document/hooks'
+import useDrawerScrollLock from '@/shared/hooks/use-drawer-scroll-lock'
+import { createPortal } from 'react-dom'
 
 interface Props {
   triggerComponent: React.ReactNode
@@ -41,6 +43,9 @@ const MoveDocumentDrawer = ({ triggerComponent, documentId, resetSwipe }: Props)
 
   const { mutate: moveDocumentMutation } = useMoveDocument(params)
 
+  // iOS Safari Drawer & Scroll 관련 버그 해결
+  useDrawerScrollLock(isOpen)
+
   const handleClickMove = () => {
     const documentIds = documentId ? [documentId] : checkDoc.getCheckedIds().map((id) => Number(id))
 
@@ -61,55 +66,73 @@ const MoveDocumentDrawer = ({ triggerComponent, documentId, resetSwipe }: Props)
   }
 
   return (
-    <Drawer open={isOpen} onOpenChange={setIsOpen}>
-      <DrawerTrigger asChild>{triggerComponent}</DrawerTrigger>
-
-      <DrawerContent
-        overlayProps={{ className: 'max-w-mobile mx-auto' }}
-        className="mx-auto h-[80dvh] max-w-mobile rounded-t-[20px]"
-      >
-        <div className="mt-[24px] flex h-[calc(100%-154px)] w-full flex-col px-[18px]">
-          <DrawerTitle className="text-title3">다른 폴더로 이동</DrawerTitle>
-
-          <Text typography="text1-medium" className="mt-[8px] text-text-sub">
-            노트를 이동시킬 폴더를 선택해주세요.
-          </Text>
-
-          <RadioGroup
-            className="mt-[24px] flex grow flex-col gap-1 overflow-y-auto"
-            defaultValue={String(selectedDirectoryId)}
-            onValueChange={(value) => setSelectedDirectoryId(Number(value))}
+    <>
+      {/* iOS Safari Drawer & Scroll 관련 버그 해결: Overlay 직접 구현 */}
+      {isOpen &&
+        createPortal(
+          <div
+            className="fixed left-0 top-0 z-50 h-dvh w-dvw"
+            onClick={() => {
+              if (isOpen) {
+                setIsOpen(false)
+              }
+            }}
           >
-            {/* 폴더 개수만큼 렌더링 */}
-            {directories.map((directory) => (
-              <div key={directory.id} className="flex items-center py-[10px]">
-                <RadioGroupItem
-                  value={String(directory.id)}
-                  id={String(directory.id)}
-                  className={cn(
-                    'mr-[12px]',
-                    selectedDirectoryId === directory.id && 'bg-fill-primary-orange border-none'
-                  )}
-                />
-                <Label
-                  htmlFor={String(directory.id)}
-                  className="cursor-pointer text-subtitle2-medium"
-                >
-                  {directory.emoji ?? ''}{' '}
-                  {directory.tag === 'DEFAULT' ? '전체 노트' : directory.name}
-                </Label>
-              </div>
-            ))}
-          </RadioGroup>
-        </div>
+            <div className="mx-auto h-dvh max-w-mobile bg-black opacity-80" />
+          </div>,
+          document.body
+        )}
 
-        <DrawerFooter className="mt-[5px]">
-          <Button variant={'largeRound'} className="w-full" onClick={handleClickMove}>
-            이동하기
-          </Button>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
+      <Drawer open={isOpen} onOpenChange={setIsOpen} modal={false}>
+        <DrawerTrigger asChild>{triggerComponent}</DrawerTrigger>
+
+        <DrawerContent
+          overlayProps={{ className: 'max-w-mobile mx-auto' }}
+          className="mx-auto h-[80dvh] max-w-mobile rounded-t-[20px]"
+        >
+          <div className="mt-[24px] flex h-[calc(100%-154px)] w-full flex-col px-[18px]">
+            <DrawerTitle className="text-title3">다른 폴더로 이동</DrawerTitle>
+
+            <Text typography="text1-medium" className="mt-[8px] text-text-sub">
+              노트를 이동시킬 폴더를 선택해주세요.
+            </Text>
+
+            <RadioGroup
+              className="mt-[24px] flex grow flex-col gap-1 overflow-y-auto"
+              defaultValue={String(selectedDirectoryId)}
+              onValueChange={(value) => setSelectedDirectoryId(Number(value))}
+            >
+              {/* 폴더 개수만큼 렌더링 */}
+              {directories.map((directory) => (
+                <div key={directory.id} className="flex items-center py-[10px]">
+                  <RadioGroupItem
+                    value={String(directory.id)}
+                    id={String(directory.id)}
+                    className={cn(
+                      'mr-[12px]',
+                      selectedDirectoryId === directory.id && 'bg-fill-primary-orange border-none'
+                    )}
+                  />
+                  <Label
+                    htmlFor={String(directory.id)}
+                    className="cursor-pointer text-subtitle2-medium"
+                  >
+                    {directory.emoji ?? ''}{' '}
+                    {directory.tag === 'DEFAULT' ? '전체 노트' : directory.name}
+                  </Label>
+                </div>
+              ))}
+            </RadioGroup>
+          </div>
+
+          <DrawerFooter className="mt-[5px]">
+            <Button variant={'largeRound'} className="w-full" onClick={handleClickMove}>
+              이동하기
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
   )
 }
 
