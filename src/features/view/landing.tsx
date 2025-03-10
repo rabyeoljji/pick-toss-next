ec57fa05ac4257806783039083d2adfd0472dc1f
rabyeoljji <nageuna922@gmail.com>
2025-03-10 12:58:53 +0000

    fix: ipados pwa debugging

--- src/features/view/landing.tsx
+++ src/features/view/landing.tsx
@@ -1,22 +1,31 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import AppStartView from './app-start'
 import { useSession } from 'next-auth/react'
 import Splash from './splash'
+import { useRouter } from 'next/navigation'
 
 // TODO: Splash 애니메이션 삽입 후 애니메이션 끝나면 AppStartView가 노출되도록
 const Landing = () => {
-  const { data: session } = useSession()
+  const { data: session, status } = useSession()
+  const router = useRouter()
+  const [isReady, setIsReady] = useState(false) // 강제 리렌더링을 위한 state
 
   // ipadOS용 route재설정
   useEffect(() => {
     if (session?.user) {
-      window.location.href = '/main'
+      setIsReady(true) // 상태 변경
     }
-  }, [session])
+  }, [session?.user])
 
-  if (!session) {
+  useEffect(() => {
+    if (isReady) {
+      router.replace('/main')
+    }
+  }, [isReady])
+
+  if (status === 'loading') {
     return <Splash />
   }
 
--- src/features/view/splash.tsx
+++ src/features/view/splash.tsx
@@ -2,11 +2,8 @@
 
 import Icon from '@/shared/components/custom/icon'
 import { motion } from 'framer-motion'
-import { useRouter } from 'next/navigation'
 
 const Splash = () => {
-  const router = useRouter()
-
   return (
     <div className="fixed right-1/2 z-50 h-dvh w-screen max-w-mobile translate-x-1/2 overflow-hidden bg-background-base-01">
       <motion.div
@@ -14,7 +11,6 @@ const Splash = () => {
         initial={{ x: 200, y: -120, opacity: 0 }}
         animate={{ x: '50%', y: '50%', opacity: 1 }}
         transition={{ duration: 0.5 }}
-        onAnimationComplete={() => router.refresh()}
       >
         <Icon name="picktoss-color" className="center size-[139px]" />
       </motion.div>
