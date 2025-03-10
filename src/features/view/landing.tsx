bb445a40857cfd8146db7cbe5d47b389d99dda05
rabyeoljji <nageuna922@gmail.com>
2025-03-10 12:12:35 +0000

    fix: ipados pwa debugging

--- src/features/view/landing.tsx
+++ src/features/view/landing.tsx
@@ -3,19 +3,17 @@
 import { useEffect } from 'react'
 import AppStartView from './app-start'
 import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
 // import Splash from './splash'
 
 // TODO: Splash 애니메이션 삽입 후 애니메이션 끝나면 AppStartView가 노출되도록
 const Landing = () => {
-  const router = useRouter()
   const { data: session } = useSession()
 
   useEffect(() => {
     if (session?.user) {
-      router.replace('/main')
+      window.location.href = '/main'
     }
-  }, [router, session])
+  }, [session])
 
   return <AppStartView />
 
