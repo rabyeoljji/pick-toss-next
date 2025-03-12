a6de1ef09e0cdc7835025e55fca4aec3af5c3512
rabyeoljji <nageuna922@gmail.com>
2025-03-12 10:57:17 +0000

    fix: start view debugging

--- src/features/advertisement/config/index.ts
+++ src/features/advertisement/config/index.ts
@@ -3,12 +3,5 @@ export const AD_ID = {
   COLLECTION1: 'DAN-g1w4RNe4b51VLcjZ',
   COLLECTION2: 'DAN-RmeNkhakPtDW83Fq',
   COLLECTION3: 'DAN-ub7XaORKQ85glmoA',
-  COLLECTION4: 'DAN-ij09nUiN6tcCI83i', // 추후 발급 시 변경
-  COLLECTION5: 'DAN-90U6leocsB4E6S10', // 추후 발급 시 변경
-  COLLECTION6: 'DAN-BYwRk8MVQXoxk6tv', // 추후 발급 시 변경
-  COLLECTION7: 'DAN-WlMZNfxummLwEEQH', // 추후 발급 시 변경
-  COLLECTION8: 'DAN-n37hM7BWkwKOk0H8', // 추후 발급 시 변경
-  COLLECTION9: 'DAN-67qUFcmyKYGYSPuY', // 추후 발급 시 변경
-  COLLECTION10: 'DAN-SuQOUxAb2pRyNpUK', // 추후 발급 시 변경
   QUIZ_SET: 'DAN-rVrn70U20nO3wv4X',
 }
