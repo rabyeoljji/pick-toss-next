54efd4b14e989912b9dfd31f4a1ae169a0fd6a25
rabyeoljji <nageuna922@gmail.com>
2025-03-13 03:31:48 +0000

    refactor: today quiz info (#352)
    * env
    
    * fix: pdfjs cmap url
    
    * fix: error
    
    * fix: cmaps
    
    * fix: cmaps error
    
    * chore: pdfjs
    
    * chore: pdfjs
    
    * chore: pdfjs
    
    * fix: pdf worker
    
    * fix: pdf worker
    
    * fix: toast border
    
    * refactor: quiz record
    
    * refactor: today quiz info
    
    ---------
    
    Co-authored-by: jw-r <evencoding@gmail.com>

--- src/features/common/metadata.ts
+++ src/features/common/metadata.ts
@@ -1,17 +1,14 @@
 import { Metadata } from 'next'
 
-/** TODO: 배포 url 변경 후 수정 필요 */
 export const Metadatas = {
   root(): Metadata {
     const baseUrl = 'https://www.picktoss.com'
 
     return {
       metadataBase: new URL(baseUrl),
-      title: '픽토스 | 나만의 AI 퀴즈',
+      title: '픽토스(picktoss) | 나만의 AI퀴즈',
       manifest: '/manifest.json',
       description: 'AI로 생성하는 나만의 퀴즈',
-      // description:
-      //   '나의 노트로부터 생성되는 다양한 퀴즈와 요점 정리를 통해 효율적으로 메타인지 할 수 있어요!',
       icons: {
         icon: '/images/picktoss-logo.png',
         apple: '/favicons/apple-touch-icon.png',
@@ -23,12 +20,12 @@ export const Metadatas = {
           url: `${baseUrl}/images/share-thumbnail.png`,
           alt: '픽토스 AI 퀴즈 썸네일',
         },
-        title: '픽토스: 나를 성장시키는 똑똑한 퀴즈',
+        title: '픽토스(picktoss) | 나만의 AI퀴즈',
         description: '픽토스에서는 AI퀴즈로 매일 간단하게 내가 배운 걸 기억할 수 있어요.',
       },
       twitter: {
-        title: '픽토스 | 나만의 AI 퀴즈',
-        description: '픽토스에서는 AI퀴즈로 매일 간단하게 내가 배운 걸 기억할 수 있어요.',
+        title: '픽토스(picktoss) | 나만의 AI퀴즈',
+        description: 'AI로 생성하는 나만의 퀴즈',
         images: [`${baseUrl}/images/share-thumbnail.png`],
       },
       alternates: {
